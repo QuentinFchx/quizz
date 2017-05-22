d328f3fe40e4130ceb4f1d4231e8ef34982cc9e5
quentin <quentin@bannerman.com>
2017-05-23 01:03:22 +0200

    feat: new manager architecture

---
+++ src/AbstractGame.ts
@@ -0,0 +1,13 @@
+export abstract class AbstractGame {
+    static title: string;
+    static rules: string;
+
+    protected output: (text: string) => void;
+    protected over: (user: any) => void;
+
+    abstract start(output: (message: string) => void, over: (user: any) => void): void;
+
+    abstract handleMessage(message: string, user: any): void;
+
+    abstract stop(): void;
+}
