0f99506f1fd1dc8456b98974eeef7e4a3d3f20b6
quentin <quentin@bannerman.com>
2017-06-02 16:27:50 +0200

    feat: mental calculations game

---
+++ src/mentalCalc/MentalCalc.ts
@@ -0,0 +1,70 @@
+import { random } from 'lodash';
+
+import { AbstractGame } from '../AbstractGame';
+
+const DURATION = 20 * 1000;
+
+export class MentalCalc extends AbstractGame {
+    static title = 'MentalCalc';
+    static rules = 'Be the first to find the solution!';
+
+    private currentDraw: {
+        numbers: number[]
+        result: number;
+    };
+    private to: NodeJS.Timer;
+
+    constructor() {
+        super();
+        this.ready = true;
+    }
+
+    start(output: (text: string) => void, over: () => void) {
+        this.output = output;
+        this.over = over;
+
+        this.drawNumbers();
+    }
+
+    handleMessage(word: string, user: any) {
+        if (!this.currentDraw) return;
+
+        const answer = parseInt(word.trim(), 10);
+        if (answer === this.currentDraw.result) {
+            this.output(`Congratulations ${user}!`);
+            this.over(user);
+        }
+    }
+
+    stop() {
+        this.clearTimers();
+        delete this.currentDraw;
+    }
+
+    private clearTimers() {
+        clearTimeout(this.to);
+        delete this.to;
+    }
+
+    private drawNumbers() {
+        const numbers = [];
+        const numbersNumber = random(3, 5);
+        for (let i = 0; i < numbersNumber; i++) {
+            numbers.push(random(100));
+        }
+        this.currentDraw = {
+            numbers,
+            result: numbers.reduce((sum, b) => sum + b)
+        };
+
+        this.output(`Solve the following:    ${numbers.join(' + ')}`);
+        this.to = setTimeout(() => {
+            this.timeout();
+        }, DURATION);
+    }
+
+    private timeout() {
+        this.output('Timeout!');
+        this.over(null);
+    }
+}
